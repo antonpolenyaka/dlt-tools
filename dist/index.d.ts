b0121a8c8fc4f3166e6e316b99197f460cab12f6
antonpolenyaka <anton@antonp.com>
2023-08-16 13:07:16 +0200

    v1.0.8

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -10,41 +10,9 @@ import CoinGeckoAPI from './apis/CoinGeckoAPI';
 import CoinMarketCapAPI from './apis/CoinMarketCapAPI';
 import EthGasStationAPI from './apis/EthGasStationAPI';
 import ExternalAPIs from './apis/ExternalAPIs';
-// Blockchain utils
 import { hasEarned, hasDesposit, sumEarnedBN, isEarnedChanged, prettyBN, getEarnedByStakingByIndex, sleep } from './blockchain/blockchainUtils';
-// Lib
 import { detectEthereumProvider, connect, checkChainId, switchChain, addChain } from './lib/connectWallet';
-// Basic
 import IWeb3Context from './IWeb3Context';
 import Web3Context from './Web3Context';
 import Web3Wallet from './Web3Wallet';
-
-export {
-    GasPriceByTypeList,
-    GasPriceByTypeRow,
-    GasPriceInfo,
-    InfoCardRow,
-    InfoCardsList,
-    MarketChartInfo,
-    MarketChartPoint,
-    TokenMarketInfo,
-    CoinGeckoAPI,
-    CoinMarketCapAPI,
-    EthGasStationAPI,
-    ExternalAPIs,
-    hasEarned,
-    hasDesposit,
-    sumEarnedBN,
-    isEarnedChanged,
-    prettyBN,
-    getEarnedByStakingByIndex,
-    sleep,
-    detectEthereumProvider,
-    connect,
-    checkChainId,
-    switchChain,
-    addChain,
-    IWeb3Context,
-    Web3Context,
-    Web3Wallet,
-};
+export { GasPriceByTypeList, GasPriceByTypeRow, GasPriceInfo, InfoCardRow, InfoCardsList, MarketChartInfo, MarketChartPoint, TokenMarketInfo, CoinGeckoAPI, CoinMarketCapAPI, EthGasStationAPI, ExternalAPIs, hasEarned, hasDesposit, sumEarnedBN, isEarnedChanged, prettyBN, getEarnedByStakingByIndex, sleep, detectEthereumProvider, connect, checkChainId, switchChain, addChain, IWeb3Context, Web3Context, Web3Wallet, };
